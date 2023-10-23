2fd2ec75ded70f78885e2b791ab1368ddb3377c1
paivoteeee <144866481+paivoteeee@users.noreply.github.com>
2023-10-22 21:19:47 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,54 @@
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('.menu')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
+
+window.sr = ScrollReveal({ reset: false});
+
+sr.reveal('.title-1', {
+    rotate: {x:36, y:0, z:0},
+    duration:3000
+});
+sr.reveal('.sub-title1', {
+    rotate:{x:0, y:36, z:0},
+    duration:3000
+});
+sr.reveal('.about', {
+    rotate:{x:10, y:0, z:0},
+    duration:3000
+});
+sr.reveal('.abo1', {
+    rotate:{x:10, y:0, z:0},
+    duration:3000
+});
+sr.reveal('.abo', {
+    rotate:{x:10, y:0, z:0},
+    duration:3000
+});
+sr.reveal('.abo2', {
+    rotate:{x:10, y:0, z:0},
+    duration:3000
+});
+sr.reveal('.abo3', {
+    rotate:{x:10, y:0, z:0},
+    duration:3000
+});
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 7000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
+
